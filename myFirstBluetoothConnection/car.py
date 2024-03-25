c11fdc28e47d5024cccf89afa370a7512f7a8c76
Siggerud <christian.siggerud@gmail.com>
2024-03-25 19:58:23 +0100

    added methods

--- myFirstBluetoothConnection/car.py
+++ myFirstBluetoothConnection/car.py
@@ -1,3 +1,4 @@
+
 from pyfirmata2 import Arduino, util
 from pynput.keyboard import Key, Listener
 from time import sleep
@@ -10,11 +11,14 @@ it.start()
 
 sleep(1)
 
+# 
+currentButtonsPressed = {'w': 0, 's': 0, 'a': 0, 'd': 0}
+
 # define pins
-pinLBNum = 6
-pinLFNum = 9
-pinRBNum = 10
-pinRFNum = 11
+pinLBNum = 11
+pinLFNum = 10
+pinRBNum = 9
+pinRFNum = 6
 
 pinLB = board.get_pin(f"d:{pinLBNum}:o")
 pinLF = board.get_pin(f"d:{pinLFNum}:o")
@@ -27,10 +31,10 @@ def advance():
     global pinRB
     global pinRF
     
-    pinRB.write(1)
-    pinRF.write(0)
-    pinLB.write(1)
-    pinLF.write(0)
+    pinRB.write(0)
+    pinRF.write(1)
+    pinLB.write(0)
+    pinLF.write(1)
     
 def back():
     global pinLB
@@ -38,10 +42,10 @@ def back():
     global pinRB
     global pinRF
     
-    pinRB.write(0)
-    pinRF.write(1)
-    pinLB.write(0)
-    pinLF.write(1)
+    pinRB.write(1)
+    pinRF.write(0)
+    pinLB.write(1)
+    pinLF.write(0)
     
 def stop():
     global pinLB
@@ -75,29 +79,95 @@ def turnLeft():
     pinRF.write(1)
     pinLB.write(1)
     pinLF.write(0)
+    
+def turnLeftWhileForward():
+    global pinLB
+    global pinLF
+    global pinRB
+    global pinRF
+
+    pinRB.write(0)
+    pinRF.write(1)
+    pinLB.write(1)
+    pinLF.write(1)
+    
+def turnRightWhileForward():
+    global pinLB
+    global pinLF
+    global pinRB
+    global pinRF
+
+    pinRB.write(1)
+    pinRF.write(1)
+    pinLB.write(0)
+    pinLF.write(1)
+    
+def turnLeftWhileBackward():
+    global pinLB
+    global pinLF
+    global pinRB
+    global pinRF
+
+    pinRB.write(1)
+    pinRF.write(0)
+    pinLB.write(1)
+    pinLF.write(1)
+
+def turnRightWhileBackward():
+    global pinLB
+    global pinLF
+    global pinRB
+    global pinRF
+
+    pinRB.write(1)
+    pinRF.write(1)
+    pinLB.write(1)
+    pinLF.write(0)
 
 # procedure for what to do when certain keys are pressed
 def on_press(key):    
+    global currentButtonsPressed
+
     # if delete is pressed, then exit thread
     if key == Key.delete:
         return False
     
     # light up yellow LED when "y" is pressed
     try:
-        if key.char == "w":
+        buttonPressed = key.char
+        if buttonPressed == "w":
             advance()
-        elif key.char == "s":
+        elif buttonPressed == "s":
             back()
-        elif key.char == "a":
-            turnLeft()
-        elif key.char == "d":
-            turnRight()
+        elif buttonPressed == "a":
+            if currentButtonsPressed['w'] == 1:
+                turnLeftWhileForward()
+            elif currentButtonsPressed['s'] == 1:
+                turnLeftWhileBackward()
+            else:
+                turnLeft()
+        elif buttonPressed == "d":
+            if currentButtonsPressed['w'] == 1:
+                turnRightWhileForward()
+            elif currentButtonsPressed['s'] == 1:
+                turnRightWhileBackward()
+            else:
+                turnRight()
+        currentButtonsPressed[buttonPressed] = 1
     except:
         stop()
 
 #procedure for what do when releasing buttons
 def on_release(key):
+    global currentButtonsPressed
+    
     stop()
+    
+    try:
+        buttonReleased = key.char
+        currentButtonsPressed[buttonReleased] = 0
+    except:
+        pass
    
 # procedure for key listening
 def get_keys():
