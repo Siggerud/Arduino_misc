cf1bb527462011862b62857b728d5f68fbf4ddb7
Siggerud <christian.siggerud@gmail.com>
2024-04-29 21:39:46 +0200

    improved GUI

--- car/sensorGUI.py
+++ car/sensorGUI.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 class SensorGUI:
     # class for creating a ski weather summary GUI
-    def __init__(self, master, temperaturePin, noisePin, lightFrontPin, lightBackPin, inputVoltage=5, readingFrequency=500):
+    def __init__(self, master, pinsInfo, inputVoltage=5, readingFrequency=500):
         self._master = master
         self._master.geometry("600x350")
         self._set_current_time_in_title()
@@ -18,38 +18,44 @@ class SensorGUI:
         self._inputVoltage = inputVoltage
         self._readingFrequency = readingFrequency
         
-        self._tempPin = temperaturePin
-        self._noisePin = noisePin
-        self._lightFrontPin = lightFrontPin
-        self._lightBackPin = lightBackPin
+        rowCount = 0
+        self._pins = []
+        self._entries = []
+        self._sensorTypes = []
+        for pinInfo in pinsInfo:
+            pin, pinTitle, sensorType = pinInfo
+            
+            self._pins.append(pin)
+            self._sensorTypes.append(sensorType)
+            
+            pinLabel = Label(master, text=pinTitle, font=regularFont)
+            pinLabel.grid(row=rowCount, column=0, sticky="w")
+            
+            pinEntry = Entry(master)
+            pinEntry.grid(row=rowCount, column=1, sticky="w")
+            self._entries.append(pinEntry)
+            
+            rowCount += 1
         
-        temperatureLabel = Label(master, text="Temperature", font=regularFont)
-        temperatureLabel.grid(row=0, column=0, sticky="w")
-        
-        self._temperatureEntry = Entry(master)
-        self._temperatureEntry.grid(row=0, column=1)
-        self._update_temperature()
-        
-        noiseLabel = Label(master, text="Noise", font=regularFont)
-        noiseLabel.grid(row=1, column=0, sticky="w")
-        
-        self._noiseEntry = Entry(master)
-        self._noiseEntry.grid(row=1, column=1)
-        self._update_noise()
-        
-        lightLabelFront = Label(master, text="Light front", font=regularFont)
-        lightLabelFront.grid(row=2, column=0, sticky="w")
-        
-        self._lightEntryFront = Entry(master)
-        self._lightEntryFront.grid(row=2, column=1)
-        self._update_light_front()
-        
-        lightLabelBack = Label(master, text="Light back", font=regularFont)
-        lightLabelBack.grid(row=3, column=0, sticky="w")
-        
-        self._lightEntryBack = Entry(master)
-        self._lightEntryBack.grid(row=3, column=1)
-        self._update_light_back()
+        self._set_current_time_in_title()
+        self._update_pins()
+            
+    def _update_pins(self):
+        for index, pin in enumerate(self._pins):
+            sensorReading = "None"
+            analogReading = pin.read()
+            if analogReading:
+                voltage = self._convert_from_analog_input_to_voltage(analogReading)
+                
+                if self._sensorTypes[index] == "Temperature":
+                    sensorReading = f"{self._get_temperature_from_voltage(voltage):.2f}"
+                else:
+                    sensorReading = f"{voltage:.2f}"
+                
+            self._entries[index].delete(0, END) # deletes the current value
+            self._entries[index].insert(0, sensorReading) # inserts the new value
+        self._master.after(self._readingFrequency, self._update_pins)
+            
         
     def _update_temperature(self):
         tempValue = "None"
@@ -95,8 +101,7 @@ class SensorGUI:
     def _convert_from_analog_input_to_voltage(self, analogValue):
         return analogValue * self._inputVoltage
         
-    def _get_temperature_from_analog_input(self, analogValue):
-        voltage = self._convert_from_analog_input_to_voltage(analogValue)
+    def _get_temperature_from_voltage(self, voltage):
         
         return (voltage - 0.5) * 100
         
@@ -112,86 +117,5 @@ class SensorGUI:
         
     
         
-        """
-        # input widgets
-        whereLabel = Label(master, text="Where do you want to ski?", font=regularFont)
-        whereLabel.grid(row=0, column=0, sticky="w", columnspan=2, pady=1, ipadx=3)
-        
-        self._places = StringVar()
-        self._placesChosen = ttk.Combobox(master, width=15, textvariable=self._places)
-        self._placesChosen["values"] = list(self._placesUrls.keys())
-        self._placesChosen.grid(row=1, column=0, pady=5, sticky="w", columnspan=2, padx=3)
-        self._placesChosen.current(0)
 
-        timeLabel = Label(master, text="When do you want to ski?", font=regularFont)
-        timeLabel.grid(row=2, column=0, sticky="w", columnspan=2, pady=1, ipadx=3)
-
-        dateLabel = Label(master, text="Date", font=regularFont)
-        dateLabel.grid(row=3, column=0)
-
-        fromHourLabel = Label(master, text="From (hour)", font=regularFont)
-        fromHourLabel.grid(row=3, column=1)
-
-        durationLabel = Label(master, text="Duration (hours)", font=regularFont)
-        durationLabel.grid(row=3, column=2)
-
-        self._day = StringVar()
-        self._dayChosen = ttk.Combobox(master, width=10, textvariable= self._day)
-        self._dayChosen["values"] = self._getDates()
-        self._dayChosen.grid(row=4, column=0, padx=3, pady=5, sticky="w")
-        self._dayChosen.current(0)
-
-        self._hour = StringVar()
-        self._hourChosen = ttk.Combobox(master, width=10, textvariable= self._hour)
-        self._hourChosen.grid(row=4, column=1, padx=10, sticky="w")
-        self._getHours()
-
-        self._dayChosen.bind("<<ComboboxSelected>>", self._setHours)
-
-        self._duration = IntVar()
-        self._durationChosen = ttk.Combobox(master, width=10, textvariable=self._duration)
-        self._durationChosen['values'] = [x for x in range(1, 31)]
-        self._durationChosen.grid(row=4, column=2)
-        self._durationChosen.current(3)
-
-        execute = Button(master, bg="cyan", text="Check", command=self._addWeatherAndLubricationInfo)
-        execute.grid(row=5, column=1)
-        
-        # output widgets
-        self._descTitleLabel = Label(master, text="Weather", font=boldFont)
-        self._descTitleLabel.grid(row=6, column=0, sticky="w", ipadx=3, pady=2)
-
-        self._descLabel = Label(master, font=regularFont)
-        self._descLabel.grid(row=6, column=1, columnspan=8, sticky="w")
-        
-        self._temperatureTitleLabel = Label(master, text = "Temperature", font=boldFont)
-        self._temperatureTitleLabel.grid(row=7, column=0, sticky="w", ipadx=3, pady=2)
-
-        self._temperatureLabel = Label(master, font=regularFont)
-        self._temperatureLabel.grid(row=7, column=1, columnspan=8, sticky="w")
-        
-        self._windTitleLabel = Label(master, text="Winds", font=boldFont)
-        self._windTitleLabel.grid(row=8, column=0, sticky="w", ipadx=3, pady=2)
-        
-        self._windLabel = Label(master, font=regularFont)
-        self._windLabel.grid(row=8, column=1, columnspan=8, sticky="w")
-
-        self._precipitationTitleLabel = Label(master, text="Precipitation", font=boldFont)
-        self._precipitationTitleLabel.grid(row=9, column=0, rowspan=3, sticky="nw", ipadx=3, pady=2)
-
-        self._precipitationLabel = Label(master, justify="left", font=regularFont)
-        self._precipitationLabel.grid(row=9, column=1, columnspan=8, sticky="w")
-        
-        self._lubricationTitleLabel = Label(master, text="Lubrication", font=boldFont)
-        self._lubricationTitleLabel.grid(row=10, column=0, sticky="w", ipadx=3, pady=2)
-
-        self._lubricationLabel = Label(master, font=regularFont)
-        self._lubricationLabel.grid(row=10, column=1, columnspan=9, sticky="w")
-        
-        self._snowDepthTitleLabel = Label(master, text="Snow depth", font=boldFont)
-        self._snowDepthTitleLabel.grid(row=11, column=0, sticky="w", ipadx=3, pady=2)
-        
-        self._snowDepthLabel = Label(master, font=regularFont)
-        self._snowDepthLabel.grid(row=11, column=1, columnspan=8, sticky="w")
-        """
     
