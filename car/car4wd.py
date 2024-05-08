4dbeab1e710ab7ff419e4d46a5c64237f19e56c5
Siggerud <christian.siggerud@gmail.com>
2024-05-08 22:29:40 +0200

    first commit

--- car/car4wd.py
+++ car/car4wd.py
@@ -1,5 +1,4 @@
 from pyfirmata2 import Arduino, util
-from pynput.keyboard import Key, Listener, KeyCode
 from time import sleep
 import controllableCar
 from threading import Thread
@@ -46,46 +45,12 @@ pinHonk = pinManager.add_digital_pin_output(pinHonkNum)
 pinObstacleSensorFront = pinManager.add_digital_pin_input(pinObstacleSensorFrontNum)
 pinObstacleSensorBack = pinManager.add_digital_pin_input(pinObstacleSensorBackNum)
 
-# procedure for what to do when certain keys are pressed
-def on_press(key):    
-    # if delete is pressed, then exit thread
-    if key == Key.delete:
-        return False
-     
-    global car
-    if type(key) == KeyCode:
-        buttonPressed = key.char
-        
-        # set which button is currently pressed
-        car.set_current_keys_pressed(buttonPressed, "pressed")
-        
-        car.drive(buttonPressed, "pressed")
-        car.turn_on_or_off_light(buttonPressed)
-        car.honk(buttonPressed, "pressed")
-        
-    elif type(key) == Key:
-        car.move_servo(key)
-
-#procedure for what do when releasing buttons
-def on_release(key):
-    global car
-
-    if type(key) == KeyCode:
-        buttonReleased = key.char
-        car.set_current_keys_pressed(buttonReleased, "released")
-        car.drive(key, "released")
-        car.honk(buttonReleased, "released")
-        
-    elif type(key) == Key:
-        pass
    
 # procedure for key listening
 def get_keys():
-    global stopThreads
-
-    with Listener(on_press=on_press, on_release = on_release) as listener:
-        listener.join() 
-        stopThreads = True
+    global car
+    
+    car.start_listening()
         
 def start_gui():
     master = Tk()
@@ -106,12 +71,9 @@ def start_gui():
     master.mainloop()
     
 
-# explanatory text
-#print("You can start steering now")
-#print("'w' for forward, 's' for backward, 'a' for left, 'd' for right")
-
 #initialize car class
-car = controllableCar.controllableCar(pinLeftBack, pinLeftForward, pinRightBack, pinRightForward)
+car = controllableCar.controllableCar("xbox")
+car.enable_driving(pinLeftBack, pinLeftForward, pinRightBack, pinRightForward)
 
 # add components to car class
 car.add_servo(pinServo)
@@ -131,7 +93,7 @@ car.add_obstacle_sensor(pinObstacleSensorFront, "front")
 car.add_obstacle_sensor(pinObstacleSensorBack, "back")
 
 # test car functions
-car.test_car_functions()
+#car.test_car_functions()
 
 # light up headlights
 car.toggle_on_light("l")
--- car/controllableCar.py
+++ car/controllableCar.py
@@ -1,23 +1,13 @@
 from pyfirmata2 import Arduino, util
-from pynput.keyboard import Key
+from pynput.keyboard import Key, KeyCode, Listener
 from time import sleep, time
+import pygame
 
 class controllableCar:
-    def __init__(self, pinLeftReverse, pinLeftForward, pinRightReverse, pinRightForward, driveCommand="w", reverseCommand="s", leftCommand="a", rightCommand="d"):
+    def __init__(self, controllerType):
         
-        # driving pins
-        self._pinLeftReverse = pinLeftReverse
-        self._pinLeftForward = pinLeftForward
-        self._pinRightReverse = pinRightReverse
-        self._pinRightForward = pinRightForward
+        self._controllerType = controllerType
         
-        # driving commands
-        self._driveCommand = driveCommand
-        self._reverseCommand = reverseCommand
-        self._leftCommand = leftCommand
-        self._rightCommand = rightCommand
-        
-        self._currentKeysPressed = {self._driveCommand: 0, self._reverseCommand: 0, self._leftCommand: 0, self._rightCommand: 0}
         self._onOffLightCommandsAndPins = {}
         self._servoSet = False
         self._lightsSet = False
@@ -38,6 +28,114 @@ class controllableCar:
         self._backObstacleSensorSet = False
         self._leftObstacleSensorSet = False
         self._rightObstacleSensorSet = False
+        
+    def enable_driving(self, pinLeftReverse, pinLeftForward, pinRightReverse, pinRightForward):
+        # driving pins
+        self._pinLeftReverse = pinLeftReverse
+        self._pinLeftForward = pinLeftForward
+        self._pinRightReverse = pinRightReverse
+        self._pinRightForward = pinRightForward
+        
+        self._driveCommand = "w"
+        self._reverseCommand = "s"
+        self._leftCommand = "a"
+        self._rightCommand = "d"
+        
+        self._currentKeysPressed = {self._driveCommand: 0, self._reverseCommand: 0, self._leftCommand: 0, self._rightCommand: 0}
+        
+    def start_listening(self):
+        if self._controllerType == "keyboard":
+            with Listener(on_press=self.on_press, on_release = self.on_release) as listener:
+                listener.join() 
+                
+        elif self._controllerType == "xbox":
+            self._set_joystick()
+            
+            print("yes")
+            pygame.init()
+            print("yes")
+            while True:
+                for event in pygame.event.get():
+                    print("yes")
+                    command = self.convert_xbox_input_to_command(event)
+                    
+                    self.drive(command)
+    
+    # TODO: check if it works in start_listening    
+    def _set_joystick(self):
+        pygame.joystick.init()
+        joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+
+        self.myJoystick = joysticks[0]
+    
+    def convert_xbox_input_to_command(self, event):
+        command = ""
+        if event.type == pygame.JOYHATMOTION:
+            horizontal, vertical = self.myJoystick.get_hat(0)
+            if horizontal == 0 and vertical == 0:
+                command = "stop"
+            elif horizontal == -1 and vertical == 1:
+                command = "left forward"
+            elif horizontal == -1 and vertical == -1:
+                command = "left backward"
+            elif horizontal == 1 and vertical == 1:
+                command = "right forward"
+            elif horizontal == 1 and vertical == -1:
+                command = "right backward"
+            elif horizontal == -1:
+                command = "left"
+            elif horizontal == 1:
+                command = "right"
+            elif vertical == -1:
+                command = "reverse"
+            elif vertical == 1:
+                command = "drive"
+        
+        elif event.type == pygame.JOYAXISMOTION:
+            valueLeftRight = self.myJoystick.get_axis(2)
+            if valueLeftRight < 0:
+                command = "moveServoLeft"
+            elif valueLeftRight > 0:
+                command = "moveServoRight"
+                
+            valueUpDown = self.myJoystick.get_axis(3)
+            if valueUpDown >= 1:
+                command = "moveServoToDefault"
+                
+        return command
+            
+            
+    # procedure for what to do when certain keys are pressed
+    def on_press(self, key):    
+        # if delete is pressed, then exit thread
+        if key == Key.delete:
+            return False
+         
+        if type(key) == KeyCode:
+            buttonPressed = key.char
+            
+            # set which button is currently pressed
+            self.set_current_keys_pressed(buttonPressed, "pressed")
+            
+            self.drive(buttonPressed, "pressed")
+            self.turn_on_or_off_light(buttonPressed)
+            self.honk(buttonPressed, "pressed")
+            
+        elif type(key) == Key:
+            self.move_servo(key)
+
+    #procedure for what do when releasing buttons
+    def on_release(self, key):
+        if type(key) == KeyCode:
+            buttonReleased = key.char
+            self.set_current_keys_pressed(buttonReleased, "released")
+            self.drive(key, "released")
+            self.honk(buttonReleased, "released")
+            
+        elif type(key) == Key:
+            pass
+            
+        
         
     def print_instructions_for_car(self):
         print("Keys and commands:")
@@ -280,58 +378,77 @@ class controllableCar:
         return not pinValue
         
     # drives according to user input, stops if car is too close too obstacle       
-    def drive(self, key, action):
-        if action == "pressed":
-            if key == self._driveCommand:
-                if self._frontObstacleSensorSet:
-                    if self._too_close(self._frontObstacleSensorPin):
-                        self._stop()
-                        return
-                        
-                    self._advance()
-            elif key == self._reverseCommand:
-                if self._backObstacleSensorSet:
-                    if self._too_close(self._backObstacleSensorPin):
-                        self._stop()
-                        return
+    def drive(self, command):
+        if command == "drive":
+            if self._frontObstacleSensorSet:
+                if self._too_close(self._frontObstacleSensorPin):
+                    self._stop()
+                    return
                     
-                    self._back()
-  
-                    if self._brakeLightsSet:
-                        self._brakeLightPin.write(1)
-                        
-                    if self._reverseSoundSet:
-                        self._make_reverse_sound()
+                self._advance()
+        elif command == "reverse":
+            if self._backObstacleSensorSet:
+                if self._too_close(self._backObstacleSensorPin):
+                    self._stop()
+                    return
                 
-            elif key == self._leftCommand:
-                if self._leftObstacleSensorSet:
-                    if self._too_close(self._leftObstacleSensorPin):
-                        self._stop()
-                        return
+                self._back()
+
+                if self._brakeLightsSet:
+                    self._brakeLightPin.write(1)
+                    
+                if self._reverseSoundSet:
+                    self._make_reverse_sound()
             
-                if self._currentKeysPressed[self._leftCommand] == 1:
-                    self._turn_left_while_forward()
-                elif self._currentKeysPressed[self._rightCommand] == 1:
-                    self._turn_left_while_backward()
-                else:
-                    self._turn_left()
-                    
-            elif key == self._rightCommand:
-                if self._rightObstacleSensorSet:
-                    if self._too_close(self._rightObstacleSensorPin):
-                        self._stop()
-                        return
+        elif command == "left":
+            if self._leftObstacleSensorSet:
+                if self._too_close(self._leftObstacleSensorPin):
+                    self._stop()
+                    return
                 
-                if self._currentKeysPressed[self._driveCommand] == 1:
-                    self._turn_right_while_forward()
-                elif self._currentKeysPressed[self._reverseCommand] == 1:
-                    self._turn_right_while_backward()
-                else:
-                    self._turn_right()
+            self._turn_left()
+        
+        elif command == "left forward":
+            if self._leftObstacleSensorSet:
+                if self._too_close(self._leftObstacleSensorPin):
+                    self._stop()
+                    return
+        
+            self._turn_left_while_forward()
+        elif command == "left reverse":
+            if self._leftObstacleSensorSet:
+                if self._too_close(self._leftObstacleSensorPin):
+                    self._stop()
+                    return
                     
-        elif action == "released":
+            self._turn_left_while_backward()
+                
+        elif command == "right":
+            if self._rightObstacleSensorSet:
+                if self._too_close(self._rightObstacleSensorPin):
+                    self._stop()
+                    return
+                    
+                self._turn_right()
+            
+        elif command == "right forward":
+            if self._rightObstacleSensorSet:
+                if self._too_close(self._rightObstacleSensorPin):
+                    self._stop()
+                    return
+            
+            self._turn_right_while_forward()
+        elif command == "right reverse":
+            if self._rightObstacleSensorSet:
+                if self._too_close(self._rightObstacleSensorPin):
+                    self._stop()
+                    return
+        
+            self._turn_right_while_backward()
+        
+        elif command == "stop":       
             self._stop()
-            
+        
             if self._brakeLightsSet:
                 self._brakeLightPin.write(0)
                 
